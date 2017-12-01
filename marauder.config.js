7b7517174e36580ff4d5ffc6dffe430f4a8107b2
Vincent Lee <vv314@foxmail.com>
2017-12-01 14:18:35 +0800

    去除无关信息

--- marauder.config.js
+++ marauder.config.js
@@ -4,17 +4,10 @@ module.exports = {
   // 出于性能考虑，babel 编译将忽略 node_modules 目录
   // 对于 es6 模块，通过 esm 配置，通知 babel 添加额外支持
   esm: [],
-  // 打包 dll(vendor) 文件
-  vendor: [],
   // 静态资源 CDN 路径
   publicPath: './',
   // 生成 js 与 css source map
   sourceMap: false,
-
-  // hybrid 上线配置，hybrid 项目请放开注释
-  // 应用此选项，生产环境下将生成 zip 包和 version 文件
-  // hybrid: {},
-
   // ftp 服务器配置
   ftp: {
     host: '',
